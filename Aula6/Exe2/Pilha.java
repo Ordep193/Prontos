c02eb9b419159bbc0bb78922af1a234d878cb779
Ordep193 <132228946+Ordep193@users.noreply.github.com>
2023-09-27 11:43:42 -0300

    Update Pilha.java

--- Aula6/Exe2/Pilha.java
+++ Aula6/Exe2/Pilha.java
@@ -2,7 +2,7 @@ package Pilha.Exe2;
 
 public class Pilha {
     static final int MAX_TAM = 100;
-    int pilha[] = new int[MAX_TAM];
+    char pilha[] = new char[MAX_TAM];
     int topo = -1;
 
     public boolean isVazia(){
@@ -11,15 +11,15 @@ public class Pilha {
     public int getTamanho(){
         return topo+1;
     }
-    public void push() throws Exception{
+    public void push(char c) throws Exception{
         if(topo==MAX_TAM-1)
             throw new Exception("Não há espaço livre");
-        pilha[++topo]=1;
+        pilha[++topo]=c;
     }
-    public void pop()throws Exception{
+    public char pop()throws Exception{
         if(isVazia())
             throw new Exception("Essa conta é inválida");
-        topo--;
+        return pilha[topo--];
     }
 }
 
