ff8d60b55129890f6ad6c82ec6b0728a62752c4d
Ordep193 <132228946+Ordep193@users.noreply.github.com>
2023-09-27 11:43:23 -0300

    Update App.java

--- Aula6/Exe2/App.java
+++ Aula6/Exe2/App.java
@@ -8,24 +8,30 @@ class App{
         Pilha p = new Pilha();
         System.out.println("Digete a conta a ser testada");
         String conta = in.nextLine();
+        boolean invalid=false;
 
         for(int i=0;i<conta.length();i++){
             if(conta.charAt(i)==('('))
                 try {
-                    p.push();    
+                    p.push(conta.charAt(i));    
                 } catch (Exception e) {
-                    System.out.println(e);
-                    break;
+                    System.out.println("Erro: "+e);
                 }
-            else if(conta.charAt(i)==')')
-                try {
+            else if(conta.charAt(i)==')'){
+                if(p.isVazia()){
+                    invalid=true;
+                }
+                try{
                     p.pop();    
                 } catch (Exception e) {
-                    System.out.println(e);
-                    break;
+                    System.out.println("Erro: "+e);
                 }
+            }
         }
-        if(!p.isVazia())
+
+        if(invalid)
+            System.out.println("Conta inválida");
+        else if(!p.isVazia())
             System.out.println("Conta inválida");
         else
             System.out.println("Conta válida");
